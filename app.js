4918b5cda5f88bfcbfcf45c2edb846e210e1e3e3
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-02-17 13:18:56 +0600

    if else added

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 document.getElementById('click-calculate').addEventListener('click',function(){
-    /*-------------------------------Call Input type ID--------------------------------------------------*/
     const food = document.getElementById('total-food').value;
     const rent = document.getElementById('total-rent').value;
     const cloth = document.getElementById('total-cloth').value;
-    /* ----------------------------- Sumation Input Type ID-------------------------------------------*/
-    const totalCalculate = parseInt(food) + parseInt(rent) + parseInt (cloth);
-    const totalExpenses  = document.getElementById('total-expenses');
-    totalExpenses.innerText = totalCalculate;
-    /*-------------------------------Update Current Balance-----------------------------------------*/
-    const totalIncome = document.getElementById('total-income').value;
-    const totalIncomeCon = parseInt(totalIncome);
-    const currentBalance = document.getElementById('current-balance');
-    const balance = totalIncomeCon - totalCalculate;
-    currentBalance.innerText = balance;
+    if(food<0 || rent<0 || cloth<0){
+        document.body.style.display=alert('please enter a positive number');
+    }
+    else{
+        const totalCalculate = parseInt(food) + parseInt(rent) + parseInt (cloth);
+        const totalExpenses  = document.getElementById('total-expenses');
+        totalExpenses.innerText = totalCalculate;
+
+        const totalIncome = document.getElementById('total-income').value;
+        const totalIncomeCon = parseInt(totalIncome);
+        
+        if(totalIncomeCon<totalCalculate){
+            document.body.style.display=alert('Your salary should be more than the total cost. Then you can see that you have the current money');
+        }
+        else{
+            const currentBalance = document.getElementById('current-balance');
+            const balance = totalIncomeCon - totalCalculate;
+            currentBalance.innerText = balance;
+        }
+    }
 });
 document.getElementById('click-save').addEventListener('click',function(){
     const currentBalance = document.getElementById('current-balance').innerText;
@@ -27,7 +36,5 @@ document.getElementById('click-save').addEventListener('click',function(){
     const totalBalance = currentBalanceCon - saveingCalculation;
     
     remainingBalance.innerText = totalBalance;
-    
+});
 
-
-})
