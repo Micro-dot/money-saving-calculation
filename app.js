0ffff0e07c43a7a48dfcd74bbbbe75f8207afb05
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-02-17 17:19:59 +0600

    add something

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ document.getElementById('click-calculate').addEventListener('click',function(){
     const food = document.getElementById('total-food').value;
     const rent = document.getElementById('total-rent').value;
     const cloth = document.getElementById('total-cloth').value;
-    if(food<0 || rent<0 || cloth<0){
-        document.body.style.display=alert('please enter a positive number');
+    if(food<0){
+        document.body.style.display=alert('please enter a positive number in food field');
     }
-    else{
-        const totalCalculate = parseInt(food) + parseInt(rent) + parseInt (cloth);
+    else if(rent<0){
+        document.body.style.display=alert('please enter a positive number in rent field');
+    }
+    else if(cloth<0){
+        document.body.style.display=alert('please enter a positive number in cloth field');
+    }
+
+    else if(food>0 || rent>0 || cloth>0){
+        const totalCalculate = parseFloat(food) + parseFloat(rent) + parseFloat (cloth);
         const totalExpenses  = document.getElementById('total-expenses');
         totalExpenses.innerText = totalCalculate;
 
         const totalIncome = document.getElementById('total-income').value;
-        const totalIncomeCon = parseInt(totalIncome);
-        
+        const totalIncomeCon = parseFloat(totalIncome);
+
         if(totalIncomeCon<totalCalculate){
             document.body.style.display=alert('Your salary should be more than the total cost. Then you can see that you have the current money');
         }
@@ -30,11 +37,27 @@ document.getElementById('click-save').addEventListener('click',function(){
     const inputSaveing = document.getElementById('input-saveing').value;
     const saveingCalculation = parseFloat(inputSaveing)/100 * currentBalanceCon;
     const totalSaveing = document.getElementById('total-saving');
-    totalSaveing.innerText = saveingCalculation;
-
-    const remainingBalance = document.getElementById('total-balance');
-    const totalBalance = currentBalanceCon - saveingCalculation;
     
-    remainingBalance.innerText = totalBalance;
+    if(currentBalanceCon<500){
+        document.body.style.display=alert('You Current balance must be grater than 500 taka. Than you can save your money');
+    }
+    else if(saveingCalculation>0){
+        totalSaveing.innerText = saveingCalculation;
+        const remainingBalance = document.getElementById('total-balance');
+        const totalBalance = currentBalanceCon - saveingCalculation;
+        remainingBalance.innerText = totalBalance;
+    }  
 });
+document.getElementById('click-reset').addEventListener('click',function(){
+    const food = document.getElementById('total-food');
+    const rent = document.getElementById('total-rent');
+    const cloth = document.getElementById('total-cloth');
+    const totalIncome = document.getElementById('total-income');
+    const inputSaveing = document.getElementById('input-saveing');
+    food.value = '';
+    rent.value='';
+    cloth.value='';
+    totalIncome.value='';
+    inputSaveing.value='';
+})
 
